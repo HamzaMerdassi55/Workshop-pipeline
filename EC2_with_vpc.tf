c3bb85ef87e55fe9031005bb5c14fa79efce4043
HamzaMerdassi55 <hamzamerdassi41@gmail.com>
2024-10-20 15:09:07 +0100

    first commit

---
+++ EC2_with_vpc.tf
@@ -0,0 +1,102 @@
+provider "aws" {
+  region="eu-west-3"
+}
+
+resource "aws_instance" "server" {
+    ami = "ami-045a8ab02aadf4f88"
+    instance_type = "t2.micro"
+    key_name = "Workshop"
+    //security_groups = [ "security_groupe" ]
+    vpc_security_groups_ids = [aws_security_group.security_groupe.id]
+    subnet_id = "aws_subnet.workshop-public_subent_01.id"
+    for_each = toset(["Jenkins-master" , "build-slave" , "ansible"])
+    tags = {
+     Name = "${each.key}"
+    }
+}
+
+resource "aws_security_group" "security_groupe" {
+  name        = "security_groupe"
+  description = "SSH Access"
+  vpc_id = aws_vpc.Workshop-vpc.id
+
+  ingress  {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
+
+resource "aws_vpc" "workshop-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "workshop-vpc"
+  }
+}
+
+resource "aws_subnet" "workshop-public_subent_01" {
+  vpc_id = aws_vpc.workshop-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "eu-west-3a"
+    tags = {
+      Name = "workshop-public_subent_01"
+    }
+}
+
+resource "aws_subnet" "workshop-public_subent_02" {
+  vpc_id = aws_vpc.workshop-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "eu-west-3b"
+    tags = {
+      Name = "workshop-public_subent_02"
+    }
+}
+
+resource "aws_internet_gateway" "workshop-igw" {
+    vpc_id = aws_vpc.workshop-vpc.id
+    tags = {
+      Name = "workshop-igw"
+    }
+}
+
+
+resource "aws_route_table" "workshop-public-rt" {
+    vpc_id = aws_vpc.workshop-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.workshop-igw.id
+    }
+    tags = {
+      Name = "workshop-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "workshop-rta-public-subent-01" {
+    subnet_id = aws_subnet.workshop-public_subent_01.id
+    route_table_id = aws_route_table.workshop-public-rt.id
+}
+
+resource "aws_route_table_association" "workshop-rta-public-subent-02" {
+    subnet_id = aws_subnet.workshop-public_subent_02.id
+    route_table_id = aws_route_table.workshop-public-rt.id
+}
+
+
+
+
+
---
+++ Readme.md
@@ -0,0 +1,6 @@
+- first step :
+    install git in your os : " https://git-scm.com/downloads/win "
+    installl vscode : " https://code.visualstudio.com/download "
+    install terraform : " https://developer.hashicorp.com/terraform/install?product_intent=terraform "
+    install aws cli : "  "
+    if you work with windows install mobaxtern to releated your ec2 with ssh and work with env linux : 
